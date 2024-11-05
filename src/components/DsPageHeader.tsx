82c924cf1e8fb6ab6d52022552e07e8b58f0a1b4
ANNA SOLECKA <ania.solecka@gmail.com>
2024-11-05 10:15:12 +0000

    Page Header Component added

---
+++ src/components/DsPageHeader.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+interface DsPageHeaderProps {
+  label?: string;
+  title: string;
+  metadataKey?: string;
+  metadataValue?: string;
+}
+const DsPageHeader: React.FC<DsPageHeaderProps> = ({
+  label,
+  title,
+  metadataKey,
+  metadataValue,
+}) => {
+  return (
+    <header className="ds_page-header">
+      <span className="ds_page-header__label  ds_content-label">{label}</span>
+      <h1 className="ds_page-header__title">{title}</h1>
+
+      {metadataKey && metadataValue && (
+        <dl className="ds_page-header__metadata  ds_metadata">
+          <div className="ds_metadata__item">
+            <dt className="ds_metadata__key">{metadataKey}</dt>
+            <dd className="ds_metadata__value">{metadataValue}</dd>
+          </div>
+        </dl>
+      )}
+    </header>
+  );
+};
+
+export default DsPageHeader;
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,10 +1,20 @@
 import ContactFormFormik from "../components/ContactFormFormik";
+import DsPageHeader from "../components/DsPageHeader";
 import LayoutComponent from "../components/LayoutComponent";
 
 const AboutPage = () => {
   return (
-    <LayoutComponent header={<ContactFormFormik />}>
-      {undefined}
+    <LayoutComponent
+      header={
+        <DsPageHeader
+          title="Contact form"
+          label="Form in formik"
+          metadataKey="Last updated"
+          metadataValue="10.12.2002"
+        />
+      }
+    >
+      <ContactFormFormik />
     </LayoutComponent>
   );
 };
