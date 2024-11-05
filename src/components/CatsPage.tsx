747b1aca13fdcb527bd1f617c7187719b94d0fff
ANNA SOLECKA <ania.solecka@gmail.com>
2024-11-05 14:34:24 +0000

    Cats table

--- src/components/CatsPage.tsx
+++ src/components/CatsPage.tsx
@@ -4,6 +4,7 @@ import { Cat } from "./CatItem";
 import { Link } from "react-router-dom";
 
 import LayoutComponent from "./LayoutComponent";
+import DsTableGrid from "../pages/DsTableGrid";
 
 function Cats() {
   const [cats, setCats] = useState<Cat[]>([]);
@@ -27,8 +28,87 @@ function Cats() {
           </li>
         ))}
       </ul>
+      <DsTableGrid
+        columns={[
+          {
+            label: "Name",
+            field: "name",
+          },
+          {
+            label: "Weight Imperial",
+            field: "weight.imperial",
+            render: (cat: any) => {
+              return <div>{cat.weight.imperial} lbs</div>;
+            },
+          },
+          {
+            label: "Weight Metric",
+            field: "weight.metric",
+            render: (cat: any) => {
+              return <div>{cat.weight.metric} kg</div>;
+            },
+          },
+
+          {
+            label: "Description",
+            field: "description",
+          },
+          {
+            label: "Adaptability",
+            field: "adaptability",
+            render: (cat: any) => {
+              return (
+                <p>
+                  {[0, 1, 2, 3, 4].map((value) => (
+                    <span
+                      key={value}
+                      className={
+                        value < cat.adaptability ? "dot active" : "dot"
+                      }
+                    ></span>
+                  ))}{" "}
+                </p>
+              );
+            },
+          },
+          {
+            label: "Child friendly",
+            field: "child_friendly",
+            render: (cat: any) => {
+              return (
+                <p>
+                  {[0, 1, 2, 3, 4].map((value) => (
+                    <span
+                      key={value}
+                      className={
+                        value < cat.stranger_friendly ? "dot active" : "dot"
+                      }
+                    ></span>
+                  ))}{" "}
+                </p>
+              );
+            },
+          },
+        ]}
+        data={cats}
+      />
     </LayoutComponent>
   );
 }
 
+// id: string;
+//   name: string;
+//   weight: {
+//     imperial: string;
+//     metric: string;
+//   };
+//   description: string;
+//   adaptability: number;
+//   child_friendly: number;
+//   energy_level: number;
+//   intelligence: number;
+//   grooming: number;
+//   health_issues: number;
+//   social_needs: number;
+//   stranger_friendly: number;
 export default Cats;
